e2688e124e61f5bdc8bb3fcb0702d33f90fcb82d
cheny-alf <1285682410@qq.com>
2021-12-31 14:28:31 +0800

    可以根据不同的服务器ip地址进行收集

--- common/common.go
+++ common/common.go
@@ -1,7 +1,26 @@
 package common
 
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
 //collectEntry 要收集的日志的配置项的结构体
 type CollectEntry struct {
 	Path  string `json:"path"`
 	Topic string `json:"topic"`
+}
+
+//获取本地的IP函数
+func GetOutboundIp() (ip string, err error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	fmt.Println(localAddr.String())
+	ip = strings.Split(localAddr.IP.String(), ":")[0]
+	return
 }
